d824fe4a00a611c26c10464e963915c082cc07ff
Quinn Narlo <105549100+pyquinnnarlo@users.noreply.github.com>
2024-04-21 23:53:49 +0000

    Update ex6.asm

--- ex6.asm
+++ ex6.asm
@@ -1,24 +1,23 @@
 ; nasm -f elf32 ex6.asm -o ex6.o
 ; ld -m elf_i386 ex6.o -o ex6
-; ./ex1
-; echo $?
+; ./ex6
 
 
 global _start
 
 section .data
     addr db "yellow"
-    len equ $-addr
+    len equ $ - addr
 
 section .text
 _start:
     mov [addr], byte 'H'
     mov [addr+5], byte '!'
-    mov eax, 4    ; sys_write system call
-    mov ebx, 1    ; stdout file descriptor
-    mov ecx, addr ; bytes to write
-    mov edx, 6    ; number of bytes to write
-    int 0x80      ; perform system call
-    mov eax, 1    ; sys_exit system call
-    mov ebx, 0    ; exit status is 0
+    mov eax, 4              ; sys_write system call
+    mov ebx, 1              ; stdout file descriptor
+    mov ecx, addr           ; bytes to write
+    mov edx, len            ; number of bytes to write
+    int 0x80                ; perform system call
+    mov eax, 1              ; sys_exit system call
+    mov ebx, 0              ; exit status is 0
     int 0x80
