44cd86e4244e157e2ce3c781ae4b7aa1fc5be08d
Quinn Narlo <105549100+pyquinnnarlo@users.noreply.github.com>
2024-04-21 18:14:13 +0000

    Update ex4.asm

--- ex4.asm
+++ ex4.asm
@@ -1,1 +1,20 @@
+; nasm -f elf32 ex1.asm -o ex4.o
+; ld -m elf_i386 ex4.o -o ex4
+; ./ex4
+; echo $?
 
+
+
+
+global _start
+_start:
+  mov eax, 1   ; exit system call
+
+  mov ebx, 13  ; error code
+  jmp goto     ; Jump to "goto" label
+
+  ; This instruction won't be executed
+  mov ebx, 4   ; change "ebx" to 4
+
+goto:
+  int 0x80     ; interrupt
